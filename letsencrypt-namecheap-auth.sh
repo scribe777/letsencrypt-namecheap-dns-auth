93632a69cab114836088a5233a14edb590a79c58
Troy A. Griffitts <scribe@crosswire.org>
2020-08-18 11:18:21 -0400

    First cut of working script

--- letsencrypt-namecheap-auth.sh
+++ letsencrypt-namecheap-auth.sh
@@ -1,5 +1,51 @@
 #!/bin/sh
 
+####### !!!!!!!!!!!!!  W A R N I N G !!!!!!!!!!!!! ####### 
+#
+#  NameCheap only has an API for setting all host DNS records
+#  i.e., we can't simply update one TXT row
+#
+# That forces the workflow of this script to:
+#	first read in all host records,
+#	leave out any old _acme-challenge records
+#	add our new certbot _acme-challenge record
+#	REPLACE ALL HOST DNS RECORDS
+#
+# This sounds dangerous and probably is!  I took a screenshot
+# of my existing NameCheap DNS entries before running this script
+# in case it didn't preserve something.
+# I personally wouldn't trust my script on a domain which had
+# more than 10 records.  This means the update URL gets pretty long
+# and AGAIN WANT TO ENCOURAGE YOU TO TAKE A SCREENSHOT OF YOUR DNS
+# CONFIGURATION AND CHECK IT AFTER RUNNING THIS SCRIPT
+#
+# ALSO, NameCheap recommends creating a sandbox account and
+# testing there.  I would suggest doing this.
+# You can run this script directly, instead of from certbot
+# if you fill in a couple extra variables, as mentioned in
+# the SANDBOX section below.
+
+# QuickStart: Don't quickstart.  Read everything up to this point first.
+#	You'll need the wget and dig commands available
+#	Enable API access on your NameCheap Account and obtain your APIKey
+#	Whitelist the IP of the server from where you will run this script
+#	Configure this script with your NameCheap userID, APIKey, and whitelisted IP address
+#
+#	TEST THIS SCRIPT BY CONFIGURING IT TO A SANDBOX ACCOUNT YOU'VE SETUP AT: https://ap.www.sandbox.namecheap.com/
+#	(see SANDBOX section below)
+#
+#	provide this script to certbox-2 when renewing, e.g.,
+#	certbot-2 renew --manual-auth-hook=/root/letsencrypt-namecheap-auth.sh
+#
+#	Probably put the above command in a monthly cron job
+#
+# Best wishes,
+#	Troy A. Griffitts <scribe@crosswire.org>
+#	https://crosswire.org
+#
+
+#
+# START OF SCARY SCRIPT ----------------------------------------------------
 #
 # -------  certbot will pass us these variables -------
 #
@@ -18,27 +64,30 @@ SERVICEURL="https://api.namecheap.com/xml.response"
 # your NameCheap login ID
 # (their docs mention both API User and NC User, but they are the same
 # in our scenario because we are editing our own records and not one of our 'clients')
-NCUSER=scribe777
+NCUSER=myUserID
 
 # your whitelisted IP address
-CLIENTIP=209.250.6.226
+CLIENTIP=8.8.8.8
 
 # your API Key
-NCAPIKEY=94072ba1d5054c75a05c3ff845ad6ef4
+NCAPIKEY=9zzzzzzzzzzzzzzzzzzzzzzzzzzzzzf4
 
-# number of seconds to wait between checks for our certbot validation records to finish propagation
-WAITSECONDS=10
-
+#
+# SANDBOX TESTING
+#
 # For sandbox testing first, you'll probably want to override some of these
 # then you can just run the script directly and see if you preserve
 # all your other host records and get the new acme validation TXT record
 # with the dummy value you specify below
 #
 #SERVICEURL="https://api.sandbox.namecheap.com/xml.response"
-#NCAPIKEY=2cac3aee097f4bc08a9b4776e2e6401c
-#CERTBOT_DOMAIN=crosswire.com
+#NCAPIKEY=2czzzzzzzzzzzzzzzzzzzzzzzzzzzz1c
+#CERTBOT_DOMAIN=crosswire.org
 #CERTBOT_VALIDATION=xyzzq
 
+
+# number of seconds to wait between checks for our certbot validation records to finish propagation
+WAITSECONDS=10
 
 # --------------- End configurable section --------------------------------
 
@@ -50,7 +99,7 @@ SLD=$(echo ${CERTBOT_DOMAIN} | rev | cut -d. -f2 | rev)
 
 
 APICOMMAND="namecheap.domains.dns.getHosts&SLD=${SLD}&TLD=${TLD}"
-wget -O getHosts.xml "${SERVICEURL}?ClientIp=${CLIENTIP}&ApiUser=${NCUSER}&ApiKey=${NCAPIKEY}&UserName=${NCUSER}&Command=${APICOMMAND}"
+wget -O /tmp/getHosts.xml "${SERVICEURL}?ClientIp=${CLIENTIP}&ApiUser=${NCUSER}&ApiKey=${NCAPIKEY}&UserName=${NCUSER}&Command=${APICOMMAND}"
 
 APICOMMAND="namecheap.domains.dns.setHosts&SLD=${SLD}&TLD=${TLD}"
 ENTRYNUM=1;
@@ -60,24 +109,28 @@ while IFS= read -r line; do
 	ADDRESS=$(echo $line|sed 's/^.* Address="\([^"]*\)".*$/\1/g')
 	MXPREF=$(echo $line|sed 's/^.* MXPref="\([^"]*\)".*$/\1/g')
 	TTL=$(echo $line|sed 's/^.* TTL="\([^"]*\)".*$/\1/g')
+
+	# apparently 1799 is "auto"
+	# if we specify what we received in getHosts, we don't preserve 'auto'
+	# so we are specifying auto here
+	TTL=1799
       
 	if [[ "${NAME}" == "_acme-challenge" ]]; then
 		# skip all existing _acme-challenge entries
 		true
 	else
-		# we're leaving off the TTL entry here and letting them all be auto; if we specify what we received, we don't preserve 'auto'
-		APICOMMAND="${APICOMMAND}&HostName${ENTRYNUM}=${NAME}&RecordType${ENTRYNUM}=${TYPE}&Address${ENTRYNUM}=${ADDRESS}&MXPref${ENTRYNUM}=${MXPREF}"
+		APICOMMAND="${APICOMMAND}&HostName${ENTRYNUM}=${NAME}&RecordType${ENTRYNUM}=${TYPE}&Address${ENTRYNUM}=${ADDRESS}&MXPref${ENTRYNUM}=${MXPREF}&TTL${ENTRYNUM}=${TTL}"
 
 		ENTRYNUM=$((${ENTRYNUM} + 1))
 	fi
-done <<< "$(grep "<host " getHosts.xml)"
+done <<< "$(grep "<host " /tmp/getHosts.xml)"
 
 
 # OK, now let's add our new acme challenge verification record
 APICOMMAND="${APICOMMAND}&HostName${ENTRYNUM}=_acme-challenge&RecordType${ENTRYNUM}=TXT&Address${ENTRYNUM}=${CERTBOT_VALIDATION}"
 
 # Finally, we'll update all host DNS records
-wget -O testapi.out "${SERVICEURL}?ClientIp=${CLIENTIP}&ApiUser=${NCUSER}&ApiKey=${NCAPIKEY}&UserName=${NCUSER}&Command=${APICOMMAND}"
+wget -O /tmp/testapi.out "${SERVICEURL}?ClientIp=${CLIENTIP}&ApiUser=${NCUSER}&ApiKey=${NCAPIKEY}&UserName=${NCUSER}&Command=${APICOMMAND}"
 
 # Actually, FINALLY, we need to wait for out records to propagate before letting certbot continue.
 FOUND=false
